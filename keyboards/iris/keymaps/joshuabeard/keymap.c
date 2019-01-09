6a7510c27b50a0283223e27094cc31e8a95e0657
joshuabeard <joshuabeard92@gmail.com>
2019-01-09 11:37:21 -0500

    my iris

--- keyboards/iris/keymaps/joshuabeard/keymap.c
+++ keyboards/iris/keymaps/joshuabeard/keymap.c
@@ -112,9 +112,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,            RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+     KC_ESC,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT,          KC_LCTL,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LSFT, KC_LALT,  KC_BSPC,                   KC_SPC,  KC_LCTL,  SECOND 
+                                      LOWER, KC_LSFT,  KC_BSPC,                    KC_SPC,  SECOND,  RAISE 
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
